4dfb924b8d704c6848acfa3b3009d854c32cc0d3
Pedro707147 <56053412+Pedro707147@users.noreply.github.com>
2019-10-02 17:13:14 -0300

    Update and rename q2.py to q3.py

--- q3.py
+++ q3.py
@@ -1,10 +1,10 @@
 def numeropi():
 
-    diferensa = 5*10**(-9)
+    diferensa = 1
     pi1 = 1
     pi2 = 1
 
-    while(diferensa <= (5*10**(-8))):
+    while(diferensa >= (5*10**(-8))):
         cont1 = 3
         cont1 += 4
         pi1 = 4*(1 - (1/3) - (1/cont1))
@@ -12,7 +12,7 @@ def numeropi():
         cont2 += 4
         pi2 = 4*(1 + (1/5) + (1/cont2))
         diferensa = pi1 + pi2
-        return diferensa
-        return pi1
-        return pi2
+    return diferensa
+    return pi1
+    return pi2
     print('Pi e igual a : ', pi2)
