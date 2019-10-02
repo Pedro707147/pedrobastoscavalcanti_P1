2c7651ab2ba81e058a0c3da5dabf44fd72a697ba
Pedro707147 <56053412+Pedro707147@users.noreply.github.com>
2019-10-02 17:08:39 -0300

    Create q4.py
    Essa eu não tinha conseguido deduzir na hora, apenas mais tarde. Mas, de qualquer forma, encontrei esse resultado.

---
+++ q4.py
@@ -0,0 +1,9 @@
+def codigo(numa, numb):
+
+    valor_a = numa//10
+    valor_b = numa % 10
+    valor_c = numb//10
+    valor_d = numb % 10
+    soma = valor_a + valor_b + valor_c + valor_d
+
+  return soma
